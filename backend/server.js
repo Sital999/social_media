0041b53e7b19c1677b1c1dbd2bf09a99a993194b
sital999 <sitalnagarkoti123@gmail.com>
2022-11-15 21:40:05 +0545

    authentication

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ const express = require("express");
 require("dotenv").config();
 const ErrorHandler = require("./middleware/errorHandleMiddleware");
 
+// for cross site request
+const cors = require("cors");
+
 const port = process.env.PORT_NUMBER || 8000;
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
